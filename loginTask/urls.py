618b9dd1bac3ee4af0fb91fdce45a24eca60a2e6
WILSON <wilsoncaballer@gmail.com>
2024-08-03 11:46:07 -0400

    corrigiendo

--- loginTask/urls.py
+++ loginTask/urls.py
@@ -4,11 +4,21 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
 
-
+# Lista de patrones de URL para la aplicación
 urlpatterns = [
-    path("", RedirectView.as_view(
-        url="accounts/", permanent=False), name="index"),
+    # Ruta para redirigir a "accounts/" en la URL raíz
+    path(
+        "",
+        RedirectView.as_view(url="accounts/", permanent=False),
+        name="index"
+    ),
+
+    # Ruta para el panel de administración de Django
     path('admin/', admin.site.urls),
+
+    # Ruta para incluir las URLs del aplicativo "tasks"
     path('tasks/', include('tasks.urls')),
+
+    # Ruta para incluir las URLs del aplicativo "accounts"
     path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
